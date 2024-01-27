8f91268e7acca90425cede17aa74a63405230352
isamyakt <isamyakt17@gmail.com>
2024-01-27 14:03:00 +0530

    constructing an event at the debug level

--- src/log/mod.rs
+++ src/log/mod.rs
@@ -7,6 +7,7 @@ use serde_json::{json, Value};
 use serde_with::skip_serializing_none;
 use std::time::{SystemTime, UNIX_EPOCH};
 use uuid::Uuid;
+use tracing::debug;
 
 #[skip_serializing_none]
 #[derive(Serialize)]
@@ -61,7 +62,7 @@ pub async fn log_request(
 		error_data,
 	};
 
-	println!("->> REQUEST LOG LINE:\n{}", json!(log_line));
+	debug!(" REQUEST LOG LINE:\n{}", json!(log_line));
 
 	// TODO - Send to cloud-watch.
 
